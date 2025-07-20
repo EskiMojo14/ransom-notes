9e0c0d996b0bc2c314d47f995a673c1ba83152c7
Ben Durrant <ben.durrant@marketdojo.com>
2025-07-20 19:58:25 +0100

    colour tweaks

--- src/features/words/Words.stories.ts
+++ src/features/words/Words.stories.ts
@@ -2,11 +2,31 @@ import type { Meta, StoryObj } from "@storybook/react-vite";
 import { words } from "./constants";
 import { Words } from "./Words";
 
+function shuffle<T>(array: Array<T>) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    const randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      array[randomIndex]!,
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      array[currentIndex]!,
+    ];
+  }
+  return array;
+}
+
 const meta = {
   component: Words,
   title: "Features/Words/Words",
   args: {
-    words: words.slice(0, 10),
+    words: shuffle(words.slice(0, 15)),
   },
 } satisfies Meta<typeof Words>;
 
