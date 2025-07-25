0e8026f0128e535fa0785bc57346f1388c3daf91
Ben Durrant <ben.durrant@marketdojo.com>
2025-07-25 18:30:42 +0100

    add reverse map to avoid .find

--- src/features/submissions/slice.ts
+++ src/features/submissions/slice.ts
@@ -1,35 +1,52 @@
 import { createSelector, createSlice } from "@reduxjs/toolkit";
 import { assert } from "@/utils";
+import { roundApi } from "../round/api";
 
 interface SubmissionState {
   rows: Array<Array<number>>;
+  // map from word indexes to their row index, to avoid needing to search every row
+  reverse: Record<number, number>;
   currentRow: number;
 }
 
 const initialState: SubmissionState = {
   rows: [[], [], []],
+  reverse: {},
   currentRow: 0,
 };
+
+function removeWord(
+  state: SubmissionState,
+  rowIndex: number,
+  wordIndex: number,
+) {
+  const row = state.rows[rowIndex];
+  assert(row, "Row should exist");
+  const wordIdx = row.indexOf(wordIndex);
+  assert(wordIdx !== -1, "Word should be in row");
+  row.splice(wordIdx, 1);
+  // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
+  delete state.reverse[wordIndex];
+}
 
 export const submissionSlice = createSlice({
   name: "submission",
   initialState,
   reducers: (create) => ({
     wordToggled: create.reducer<number>((state, { payload }) => {
-      const row = state.rows.find((row) => row.includes(payload));
-      if (row) {
-        row.splice(row.indexOf(payload), 1);
+      const rowIdx = state.reverse[payload];
+      if (typeof rowIdx === "number") {
+        removeWord(state, rowIdx, payload);
       } else {
         const currentRow = state.rows[state.currentRow];
         assert(currentRow, "Current row should exist");
         currentRow.push(payload);
+        state.reverse[payload] = state.currentRow;
       }
     }),
     wordDeselected: create.reducer<{ rowIndex: number; wordIndex: number }>(
-      (state, { payload }) => {
-        const row = state.rows[payload.rowIndex];
-        assert(row, "Row should exist");
-        row.splice(row.indexOf(payload.wordIndex), 1);
+      (state, { payload: { rowIndex, wordIndex } }) => {
+        removeWord(state, rowIndex, wordIndex);
       },
     ),
     rowSelected: create.reducer<number>((state, action) => {
@@ -41,10 +58,23 @@ export const submissionSlice = createSlice({
       state.currentRow = state.rows.length - 1;
     }),
     rowRemoved: create.reducer<number>((state, { payload }) => {
+      const row = state.rows[payload];
+      assert(row, "Row should exist");
+      row.forEach((wordIndex) => {
+        // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
+        delete state.reverse[wordIndex];
+      });
       state.rows.splice(payload, 1);
       state.currentRow = Math.min(state.currentRow, state.rows.length - 1);
     }),
   }),
+  extraReducers(builder) {
+    // our indexes will be out of date, clear submission
+    builder.addMatcher(
+      roundApi.endpoints.getWordPool.matchFulfilled,
+      () => initialState,
+    );
+  },
   selectors: {
     selectRows: (state) => state.rows,
     selectCurrentRow: (state) => state.currentRow,
