b5f8a79d050196b3753cfc27024f14c30930527c
Ben Durrant <ben.durrant@marketdojo.com>
2025-08-13 17:27:05 +0100

    don't use tags for chat

--- src/features/chat/api.ts
+++ src/features/chat/api.ts
@@ -27,127 +27,121 @@ const messageAdapter = createEntityAdapter<Message>({
   sortComparer: (a, b) => a.created_at.localeCompare(b.created_at),
 });
 
-export const chatApi = api
-  .enhanceEndpoints({ addTagTypes: ["Chat"] })
-  .injectEndpoints({
-    endpoints: (build) => ({
-      getChatMessages: build.query<
-        EntityState<Message, Message["id"]>,
-        Game["id"],
-        Array<Message>
-      >({
-        queryFn: supabaseQueryFn({
-          query: (gameId) =>
-            supabase
-              .from("messages")
-              .select(messageSelect)
-              .eq("game_id", gameId),
-          transformResponse: (messages) =>
-            messageAdapter.getInitialState(undefined, messages),
-        }),
-        providesTags: (_res, _err, gameId) => [{ type: "Chat", id: gameId }],
-        async onCacheEntryAdded(
-          gameId,
-          // eslint-disable-next-line @typescript-eslint/unbound-method
-          { dispatch, updateCachedData, cacheDataLoaded, cacheEntryRemoved },
-        ) {
-          try {
-            await cacheDataLoaded;
-            const subscriptions: Array<{ unsubscribe: () => void }> = [];
-            const channel = await listenTo(
-              "messages",
-              {
-                async insert({ new: { user_id, id, created_at, message } }) {
-                  const sub = dispatch(
-                    profileApi.endpoints.getProfile.initiate(user_id),
-                  );
-                  subscriptions.push(sub);
-                  const author = await sub.unwrap();
-                  updateCachedData((draft) =>
-                    messageAdapter.addOne(draft, {
-                      id,
-                      created_at,
-                      message,
-                      user_id,
-                      author,
-                    }),
-                  );
-                },
-                update({ new: { id, message } }) {
-                  updateCachedData((draft) =>
-                    messageAdapter.updateOne(draft, {
-                      id,
-                      changes: { message },
-                    }),
-                  );
-                },
-                delete({ old: { id } }) {
-                  if (id) {
-                    updateCachedData((draft) =>
-                      messageAdapter.removeOne(draft, id),
-                    );
-                  }
-                },
-              },
-              `game_id=eq.${gameId}`,
-            );
-            await cacheEntryRemoved;
-            await channel.unsubscribe();
-            for (const sub of subscriptions) sub.unsubscribe();
-          } catch {
-            // cacheDataLoaded throws if the cache entry is removed before the promise resolves
-          }
-        },
+export const chatApi = api.injectEndpoints({
+  endpoints: (build) => ({
+    getChatMessages: build.query<
+      EntityState<Message, Message["id"]>,
+      Game["id"],
+      Array<Message>
+    >({
+      queryFn: supabaseQueryFn({
+        query: (gameId) =>
+          supabase.from("messages").select(messageSelect).eq("game_id", gameId),
+        transformResponse: (messages) =>
+          messageAdapter.getInitialState(undefined, messages),
       }),
-      sendChatMessage: build.mutation<
-        Message,
-        { gameId: Game["id"]; userId: string; message: string },
-        Message
-      >({
-        queryFn: supabaseQueryFn(({ gameId, userId, message }) =>
-          supabase
-            .from("messages")
-            .insert({ game_id: gameId, user_id: userId, message })
-            .select(messageSelect)
-            .single(),
-        ),
-        async onQueryStarted(
-          { gameId, userId, message },
-          { dispatch, queryFulfilled },
-        ) {
-          const tempId = -randomInt(20);
-          const profile = await dispatch(
-            profileApi.endpoints.getProfile.initiate(userId),
-          ).unwrap();
-          const { undo } = dispatch(
+      async onCacheEntryAdded(
+        gameId,
+        // eslint-disable-next-line @typescript-eslint/unbound-method
+        { dispatch, updateCachedData, cacheDataLoaded, cacheEntryRemoved },
+      ) {
+        try {
+          await cacheDataLoaded;
+          const subscriptions: Array<{ unsubscribe: () => void }> = [];
+          const channel = await listenTo(
+            "messages",
+            {
+              async insert({ new: { user_id, id, created_at, message } }) {
+                const sub = dispatch(
+                  profileApi.endpoints.getProfile.initiate(user_id),
+                );
+                subscriptions.push(sub);
+                const author = await sub.unwrap();
+                updateCachedData((draft) =>
+                  messageAdapter.addOne(draft, {
+                    id,
+                    created_at,
+                    message,
+                    user_id,
+                    author,
+                  }),
+                );
+              },
+              update({ new: { id, message } }) {
+                updateCachedData((draft) =>
+                  messageAdapter.updateOne(draft, {
+                    id,
+                    changes: { message },
+                  }),
+                );
+              },
+              delete({ old: { id } }) {
+                if (id) {
+                  updateCachedData((draft) =>
+                    messageAdapter.removeOne(draft, id),
+                  );
+                }
+              },
+            },
+            `game_id=eq.${gameId}`,
+          );
+          await cacheEntryRemoved;
+          await channel.unsubscribe();
+          for (const sub of subscriptions) sub.unsubscribe();
+        } catch {
+          // cacheDataLoaded throws if the cache entry is removed before the promise resolves
+        }
+      },
+    }),
+    sendChatMessage: build.mutation<
+      Message,
+      { gameId: Game["id"]; userId: string; message: string },
+      Message
+    >({
+      queryFn: supabaseQueryFn(({ gameId, userId, message }) =>
+        supabase
+          .from("messages")
+          .insert({ game_id: gameId, user_id: userId, message })
+          .select(messageSelect)
+          .single(),
+      ),
+      async onQueryStarted(
+        { gameId, userId, message },
+        { dispatch, queryFulfilled },
+      ) {
+        const tempId = -randomInt(20);
+        const profile = await dispatch(
+          profileApi.endpoints.getProfile.initiate(userId),
+        ).unwrap();
+        const { undo } = dispatch(
+          chatApi.util.updateQueryData("getChatMessages", gameId, (draft) =>
+            messageAdapter.addOne(draft, {
+              id: tempId,
+              created_at: new Date().toISOString(),
+              message,
+              user_id: userId,
+              author: profile,
+            }),
+          ),
+        );
+        try {
+          const { data } = await queryFulfilled;
+          dispatch(
             chatApi.util.updateQueryData("getChatMessages", gameId, (draft) =>
-              messageAdapter.addOne(draft, {
+              messageAdapter.updateOne(draft, {
                 id: tempId,
-                created_at: new Date().toISOString(),
-                message,
-                user_id: userId,
-                author: profile,
+                changes: data,
               }),
             ),
           );
-          try {
-            const { data } = await queryFulfilled;
-            dispatch(
-              chatApi.util.updateQueryData("getChatMessages", gameId, (draft) =>
-                messageAdapter.updateOne(draft, {
-                  id: tempId,
-                  changes: data,
-                }),
-              ),
-            );
-          } catch {
-            undo();
-          }
-        },
-        extraOptions: { minimumPendingTime: false },
-      }),
+        } catch {
+          undo();
+        }
+      },
+      extraOptions: { minimumPendingTime: false },
     }),
-  });
+  }),
+});
 
 export const { useGetChatMessagesQuery, useSendChatMessageMutation } = chatApi;
 
